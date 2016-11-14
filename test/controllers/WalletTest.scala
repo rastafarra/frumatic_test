85e908775416e6186177808ad2f5b85b5b0b40eb
Denis Smolyar <rastafarra@gmail.com>
2016-11-14 15:09:55 +0500

    * fix for more literary

--- test/controllers/WalletTest.scala
+++ test/controllers/WalletTest.scala
@@ -9,40 +9,41 @@ class WalletTest extends PlaySpec with OneAppPerSuite {
   val acc1 = "111"
   val acc2 = "222"
 
+
   "Index" should {
-    "not implemented" in {
+    "should not be implemented" in {
       status (route (app, FakeRequest(GET, "/")).get) mustBe NOT_IMPLEMENTED
     }
   }
 
   "Amount" should {
-    "not found by defaul" in {
+    "should not be found by default" in {
       contentAsString(route(app, FakeRequest(GET, s"/accounts/$acc1")).get) mustBe "NotFound"
       contentAsString(route(app, FakeRequest(GET, s"/accounts/$acc2")).get) mustBe "NotFound"
     }
   }
 
   "Add" should {
-    "added value" in {
+    "should add the value and respond with ok" in {
       contentAsString(route(app, FakeRequest(POST, s"/add?account=$acc1&summ=100")).get) mustBe "Amount(100)"
       contentAsString(route(app, FakeRequest(POST, s"/add?account=$acc2&summ=100")).get) mustBe "Amount(100)"
     }
   }
 
   "Move ok" should {
-    "move" in {
+    "should move value to another account and respond with ok" in {
       contentAsString(route(app, FakeRequest(POST, s"/move?from=$acc1&to=$acc2&summ=50")).get) mustBe "Amount"
     }
   }
 
-  "Move with NotEnoughMoney" should {
-    "move error" in {
+  "Move with not enogh money" should {
+    "should respond with  error NotEnoughMoney" in {
       contentAsString(route(app, FakeRequest(POST, s"/move?from=$acc1&to=$acc2&summ=150")).get) mustBe "NotEnoughMoney"
     }
   }
 
   "Amount" should {
-    "get answer" in {
+    "should return proper amount" in {
       contentAsString(route(app, FakeRequest(GET, s"/accounts/$acc1")).get) mustBe "Amount(50)"
       contentAsString(route(app, FakeRequest(GET, s"/accounts/$acc2")).get) mustBe "Amount(150)"
     }
